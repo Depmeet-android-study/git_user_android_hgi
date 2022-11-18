b1033c20d67cf6c750b3c501c2c21e6e98a9c7b6
Gyuil-Hwang <hwanggyuil@gmail.com>
2022-11-19 01:13:24 +0900

    feat. Base > 베이스 세팅 추가

---
+++ presentation/src/main/java/com/example/presentation/base/BaseFragment.kt
@@ -0,0 +1,157 @@
+package com.example.presentation.base
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.DialogFragmentNavigator
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.findNavController
+import com.example.presentation.util.LoadingDialog
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+abstract class BaseFragment<T : ViewDataBinding, R : BaseViewModel>(layoutId: Int) : Fragment(layoutId) {
+
+    private var _binding: T? = null
+    val binding get()= requireNotNull(_binding)
+
+    /**
+     * setContentView로 호출할 Layout의 리소스 Id.
+     * ex) R.layout.activity_main
+     */
+    abstract val layoutResourceId: Int
+
+    /**
+     * viewModel 로 쓰일 변수.
+     */
+    abstract val viewModel: R
+
+    /**
+     * 두번째로 호출.
+     * 데이터 바인딩 및 Coroutine 설정.
+     * ex) lifecyelScope.launch{}, lifecycleScope.launchWhenStarted{] ..
+     */
+    abstract fun initStartView()
+
+    /**
+     * 두번째로 호출.
+     * 데이터 바인딩 및 rxjava 설정.
+     * ex) rxjava observe, databinding observe..
+     */
+    abstract fun initDataBinding()
+
+    /**
+     * 바인딩 이후에 할 일을 여기에 구현.
+     * 그 외에 설정할 것이 있으면 이곳에서 설정.
+     * 클릭 리스너도 이곳에서 설정.
+     */
+    abstract fun initAfterBinding()
+
+    private var isSetBackButtonValid = false
+
+    /**
+     * Loading Dialog 관련해서 사용할 변수
+     */
+    private val mLoadingDialog: LoadingDialog by lazy { LoadingDialog(requireContext()) }
+
+    /**
+     * Exception을 처리할 SharedFlow
+    */
+    protected var exception: SharedFlow<Throwable>? = null
+    private var toast: Toast? = null
+
+    init {
+        lifecycleScope.launchWhenStarted {
+            launch {
+                exception?.collectLatest { exception ->
+                    showToastMessage(exception)
+                }
+            }
+
+            launch {
+                viewModel.errorEvent.collectLatest { e ->
+                    dismissLoadingDialog()
+                    showToastMessage(e)
+                }
+            }
+
+            launch {
+                viewModel.loadingEvent.collectLatest {
+                    if(!it) { showLoadingDialog() }
+                    else { dismissLoadingDialog() }
+                }
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutResourceId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        initStartView()
+        initDataBinding()
+        initAfterBinding()
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        toast?.cancel()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    // 로딩 다이얼로그, 즉 로딩창을 띄워줌.
+    // 네트워크가 시작될 때 사용자가 무작정 기다리게 하지 않기 위해 작성.
+    private fun showLoadingDialog() {
+        mLoadingDialog.show()
+    }
+
+    // 띄워 놓은 로딩 다이얼로그를 없앰.
+    private fun dismissLoadingDialog() {
+        if (mLoadingDialog.isShowing) {
+            mLoadingDialog.dismiss()
+        }
+    }
+
+    // Toast Message 관련 함수
+    protected fun showToastMessage(e: Throwable?) {
+        toast?.cancel()
+        toast = Toast.makeText(activity, e?.message, Toast.LENGTH_SHORT)?.apply { show() }
+    }
+
+    // Toast Message 관련 함수
+    protected fun toastMessage(message: String) {
+        toast?.cancel()
+        toast = Toast.makeText(activity, message, Toast.LENGTH_SHORT)?.apply { show() }
+    }
+
+    // navigation 중복체크 관리 <- checkNavigation 대신 사용할것
+    protected fun Fragment.navigate(directions: NavDirections) {
+        val controller = findNavController()
+        val currentDestination = (controller.currentDestination as? FragmentNavigator.Destination)?.className
+            ?: (controller.currentDestination as? DialogFragmentNavigator.Destination)?.className
+        if (currentDestination == this.javaClass.name) {
+            controller.navigate(directions)
+        }
+    }
+}
+
---
+++ presentation/src/main/java/com/example/presentation/base/BaseViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.presentation.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val _errorEvent: MutableSharedFlow<Throwable> = MutableSharedFlow()
+    val errorEvent: SharedFlow<Throwable> = _errorEvent.asSharedFlow()
+
+    private val _loadingEvent: MutableSharedFlow<Boolean> = MutableSharedFlow()
+    val loadingEvent: SharedFlow<Boolean> = _loadingEvent.asSharedFlow()
+
+    private val errorHandler = CoroutineExceptionHandler { CoroutineContext, throwable ->
+        viewModelScope.launch(CoroutineContext) {
+            _errorEvent.emit(throwable)
+        }
+    }
+
+    private val _needLoginEvent: MutableSharedFlow<Boolean> = MutableSharedFlow<Boolean>()
+    val needLoginEvent: SharedFlow<Boolean> = _needLoginEvent
+
+    fun catchError(e: Throwable?) {
+        viewModelScope.launch(errorHandler) {
+            e?.let { exception ->
+                _errorEvent.emit(exception)
+            }
+            dismissLoading()
+        }
+    }
+
+    fun showLoading() {
+        baseViewModelScope.launch {
+            _loadingEvent.emit(false)
+        }
+    }
+
+    fun dismissLoading() {
+        baseViewModelScope.launch {
+            _loadingEvent.emit(true)
+        }
+    }
+
+    protected val baseViewModelScope: CoroutineScope = viewModelScope + errorHandler
+}
